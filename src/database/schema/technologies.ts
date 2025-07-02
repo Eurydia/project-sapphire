0af2793e0ad5aa90c02bef96ac5fe3f240b848cc
Eurydia <crossdeltavi@gmail.com>
2025-07-02 15:00:36 +0200

    Initial commit
    Project scaffolding and initial file setup.

--- src/database/schema/technologies.ts
+++
@@ -1,8 +0,0 @@
-import * as t from "drizzle-orm/sqlite-core";
-import { sqliteTable } from "drizzle-orm/sqlite-core";
-
-export const technologiesTable = sqliteTable("technologies", {
-  id: t.integer().primaryKey(),
-  name: t.text().notNull(),
-  color: t.text().notNull(),
-});
