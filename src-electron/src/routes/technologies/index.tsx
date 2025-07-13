05afa8f4d4e0dd1da7dea40cb1f9a74eba8ae284
Eurydia <crossdeltavi@gmail.com>
2025-07-13 12:11:25 +0200

    Restructure project directories and cleanup
    Moved and renamed core configuration and source files to the project root, consolidated database and renderer modules, and removed obsolete src-client and src-electron directories. Updated .gitignore and added VSCode settings for improved development environment consistency.

--- src-electron/src/routes/technologies/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/technologies/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/technoloies/"!</div>
-}
--- src-electron/src/types/generic.ts
+++
@@ -1,1 +0,0 @@
-export type Maybe<TTa, TTb> = { ok: true; data: TTa } | { ok: null; other: TTb }
