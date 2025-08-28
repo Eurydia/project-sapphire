8e77305a8f96bd14a5e54b9146ef17cd1cf03750
Eurydia <crossdeltavi@gmail.com>
2025-08-28 23:40:17 +0200

    Remove custom link palette and typography from theme
    Eliminated the custom 'link' palette and typography variants from the theme definition. Updated the styled-link component to no longer rely on theme-based link colors, simplifying link styling.

--- src/renderer/app/theme.ts
+++ src/renderer/app/theme.ts
@@ -1,38 +1,6 @@
-import { alpha, createTheme, lighten } from "@mui/material"
-
-declare module "@mui/material/styles" {
-  interface Palette {
-    link: {
-      normal: string
-      visited: string
-      hover: string
-    }
-  }
-
-  interface PaletteOptions {
-    link?: {
-      normal: string
-      visited: string
-      hover: string
-    }
-  }
-  interface TypographyVariantsOptions {
-    monospaceFontFamily: string
-    serifFontFamily: string
-  }
-
-  interface TypographyVariants {
-    monospaceFontFamily: string
-    serifFontFamily: string
-  }
-}
+import { alpha, createTheme } from "@mui/material"
 
 export const theme = createTheme({
-  typography: {
-    monospaceFontFamily: '"Fira Code"',
-    serifFontFamily: '"Fira Code"',
-    fontFamily: '"Fira Code"',
-  },
   components: {
     MuiTypography: {
       styleOverrides: {
@@ -73,11 +41,6 @@ export const theme = createTheme({
       primary: alpha("#fff", 0.8),
       secondary: alpha("#fff", 0.7),
       disabled: alpha("#fff", 0.6),
-    },
-    link: {
-      normal: alpha("#fff", 0.8),
-      visited: alpha("#fff", 0.8),
-      hover: alpha(lighten("#696969", 0.5), 0.8),
     },
   },
 })
--- src/renderer/components/navigation/styled-link.tsx
+++ src/renderer/components/navigation/styled-link.tsx
@@ -1,5 +1,5 @@
 import type { LinkProps } from "@mui/material"
-import { Link, useTheme } from "@mui/material"
+import { Link } from "@mui/material"
 import type { LinkComponent } from "@tanstack/react-router"
 import { createLink } from "@tanstack/react-router"
 import React from "react"
@@ -8,27 +8,12 @@ const MUILinkComponent = React.forwardRef<
   HTMLAnchorElement,
   LinkProps
 >(({ sx, ...props }, ref) => {
-  const {
-    palette: {
-      link: { hover, normal, visited },
-    },
-  } = useTheme()
   return (
     <Link
       ref={ref}
       {...props}
       sx={{
-        color: normal,
-        textDecorationColor: normal,
         textDecorationLine: "none",
-        "&:visited": {
-          textDecorationColor: visited,
-          color: visited,
-        },
-        "&:hover": {
-          textDecorationColor: hover,
-          color: hover,
-        },
         ...sx,
       }}
     />
